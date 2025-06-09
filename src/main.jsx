e6bcdede7e3a6383774964047c6c22541ec005f1
Anisa Febriyani17 <anisa.febriyani17@gmail.com>
2025-06-09 22:45:56 +0700

    revision : header, pages login and register with tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import LoginPage from "./pages/loginPage.jsx";
+import RegisterPage from "./pages/registerPage.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <LoginPage />
+    <RegisterPage />
   </StrictMode>
 );
