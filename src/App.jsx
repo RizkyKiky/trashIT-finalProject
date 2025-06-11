7b486ef232b24762be0c083b04abee421f08f754
Anisa Febriyani17 <anisa.febriyani17@gmail.com>
2025-06-11 15:03:32 +0700

    create Dashboard user and admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
