06c9caee24a1c625bbdb79f5fb3a1bff94b2c6db
RizkyKiky <samrizky345@gmail.com>
2025-06-15 00:53:53 +0700

    add : bar chart component

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { Card, CardBody, CardHeader, Typography } from '@material-tailwind/react';
+import Chart from 'react-apexcharts';
+import { ChartBarIcon } from '@heroicons/react/24/outline';
+
+export default function BarChart() {
+  const chartConfig = {
+    type: 'bar',
+    height: 240,
+    series: [
+      {
+        name: 'Sales',
+        data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+      },
+    ],
+    options: {
+      chart: {
+        toolbar: {
+          show: false,
+        },
+      },
+      title: {
+        show: '',
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ['#020617'],
+      plotOptions: {
+        bar: {
+          columnWidth: '40%',
+          borderRadius: 2,
+        },
+      },
+      xaxis: {
+        axisTicks: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        labels: {
+          style: {
+            colors: '#FFFFFF',
+            fontSize: '12px',
+            fontFamily: 'inherit',
+            fontWeight: 400,
+          },
+        },
+        categories: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: '#FFFFFF',
+            fontSize: '12px',
+            fontFamily: 'inherit',
+            fontWeight: 400,
+          },
+        },
+      },
+      grid: {
+        show: true,
+        borderColor: '#dddddd',
+        strokeDashArray: 5,
+        xaxis: {
+          lines: {
+            show: true,
+          },
+        },
+        padding: {
+          top: 5,
+          right: 20,
+        },
+      },
+      fill: {
+        opacity: 0.8,
+      },
+      tooltip: {
+        theme: 'dark',
+      },
+    },
+  };
+  return (
+    <Card className="card bg-primary">
+      <CardHeader floated={false} shadow={false} color="transparent" className="flex flex-col gap-4 rounded-none md:flex-row md:items-center">
+        <div className="w-max rounded-lg bg-gray-900 p-5 text-white">
+          <ChartBarIcon className="h-6 w-6" />
+        </div>
+        <div>
+          <Typography variant="h6" color="blue-gray">
+            Monthly Deposit
+          </Typography>
+          <Typography variant="small" color="gray" className="max-w-sm font-normal">
+            Monthly deposits are data on every deposit that has been approved each month.
+          </Typography>
+        </div>
+      </CardHeader>
+      <CardBody className="px-2 pb-0">
+        <Chart {...chartConfig} />
+      </CardBody>
+    </Card>
+  );
+}
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Link, Route } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
+import BarChart from '../components/BarChart';
 
 export default function AdminDashboard() {
   return (
@@ -13,31 +14,25 @@ export default function AdminDashboard() {
         <div className="flex gap-x-4">
           <div className="card bg-primary text-primary-content w-96">
             <div className="card-body">
-              <h2 className="card-title">Card 1</h2>
+              <h2 className="card-title">Total Pengguna</h2>
               <p>Card 1 content</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
             </div>
           </div>
           <div className="card bg-primary text-primary-content w-96">
             <div className="card-body">
-              <h2 className="card-title">Card 2</h2>
+              <h2 className="card-title">Total Transaksi</h2>
               <p>Card 2 content</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
             </div>
           </div>
           <div className="card bg-primary text-primary-content w-96">
             <div className="card-body">
-              <h2 className="card-title">Card 3</h2>
+              <h2 className="card-title">Total Approvals</h2>
               <p>Card 3 content</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
             </div>
           </div>
+        </div>
+        <div className="mt-6">
+          <BarChart />
         </div>
       </div>
     </div>
